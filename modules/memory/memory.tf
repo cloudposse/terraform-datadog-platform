f7d182de5dd4b24f4ca0d3b4ff21ddc4f3a7942b
aknysh <aknysh>
2017-12-21 21:48:08 -0500

    Add `memory` module

---
+++ modules/memory/memory.tf
@@ -0,0 +1,30 @@
+resource "datadog_monitor" "memory_usage" {
+  count = "${var.monitor_enabled == "true" ? 1 : 0}"
+  name  = "[${module.label.id}] memory usage"
+  type  = "${var.alert_type}"
+
+  message = <<EOF
+  Memory usage for last ${var.period} on host {host.name} ({host.ip})
+  ${var.remediation}
+  ${var.notify}
+  EOF
+
+  escalation_message = "Memory usage on host {host.name} ({host.ip}) ${var.escalation_notify}"
+  query              = "avg(last_${var.period}):avg:system.mem.free{${join(",", compact(var.selector))}} by ${var.group_by} > ${var.critical_threshold}"
+
+  thresholds {
+    ok       = "${var.ok_threshold}"
+    warning  = "${var.warning_threshold}"
+    critical = "${var.critical_threshold}"
+  }
+
+  renotify_interval = "${var.renotify_interval}"
+  new_host_delay    = "${var.new_host_delay}"
+  notify_no_data    = "${var.notify_no_data}"
+
+  silenced {
+    "*" = "${var.monitor_silenced}"
+  }
+
+  tags = "${var.datadog_monitor_tags}"
+}
