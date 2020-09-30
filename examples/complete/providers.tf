468ccab9bae71cfa66ad838f7531c2abea65535e
Andriy Knysh <aknysh@users.noreply.github.com>
2020-09-30 18:46:29 -0400

    Update examples/complete/providers.tf
    Co-authored-by: Erik Osterman <erik@cloudposse.com>

--- examples/complete/providers.tf
+++ examples/complete/providers.tf
@@ -1,4 +1,4 @@
-# For the example, we use ENV variables `DD_API_KEY ` and `DD_APP_KEY` to specify `api_key` and `app_key` to the provider
+# For the example, we use ENV variables `DD_API_KEY` and `DD_APP_KEY` in place of the `api_key` and `app_key` parameters to the provider
 # https://registry.terraform.io/providers/DataDog/datadog/latest/docs#argument-reference
 provider "datadog" {
 }
