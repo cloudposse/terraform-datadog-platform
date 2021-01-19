0f491d7067e97645a0971732dc23c200c723d5b0
Matt Gowie <matt@masterpoint.io>
2021-01-19 07:57:58 -0700

    feat: adds initial cut at datadog_synthetics_test resource (#25)
    Co-authored-by: Andriy Knysh <aknysh@users.noreply.github.com>
    Co-authored-by: actions-bot <58130806+actions-bot@users.noreply.github.com>

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,5 +1,6 @@
-module "yaml_config" {
-  source = "git::https://github.com/cloudposse/terraform-yaml-config.git?ref=tags/0.1.0"
+module "monitor_configs" {
+  source  = "cloudposse/config/yaml"
+  version = "0.6.0"
 
   map_config_local_base_path = path.module
   map_config_paths           = var.monitor_paths
@@ -7,10 +8,21 @@ module "yaml_config" {
   context = module.this.context
 }
 
+module "synthetic_configs" {
+  source  = "cloudposse/config/yaml"
+  version = "0.6.0"
+
+  map_config_local_base_path = path.module
+  map_config_paths           = var.synthetic_paths
+
+  context = module.this.context
+}
+
 module "datadog_monitors" {
   source = "../../"
 
-  datadog_monitors     = module.yaml_config.map_configs
+  datadog_monitors     = module.monitor_configs.map_configs
+  datadog_synthetics   = module.synthetic_configs.map_configs
   alert_tags           = var.alert_tags
   alert_tags_separator = var.alert_tags_separator
 
