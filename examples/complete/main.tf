c67a07df99fab8f7281fc324f0c5f3221c1400d4
aknysh <andriy.knysh@gmail.com>
2021-08-29 11:25:34 -0400

    Add monitor roles

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -18,6 +18,19 @@ module "synthetic_configs" {
   context = module.this.context
 }
 
+# https://registry.terraform.io/providers/DataDog/datadog/latest/docs/resources/role
+# https://registry.terraform.io/providers/DataDog/datadog/latest/docs/data-sources/permissions
+# https://registry.terraform.io/providers/DataDog/datadog/latest/docs/resources/monitor
+# https://docs.datadoghq.com/account_management/rbac/?tab=datadogapplication
+# https://docs.datadoghq.com/account_management/rbac/permissions/
+# https://docs.datadoghq.com/api/latest/monitors/
+# https://github.com/hashicorp/cdktf-provider-datadog/blob/main/API.md
+
+# Get all available Datadog permissions
+data "datadog_permissions" "permissions" {}
+
+# Create separate roles for each monitor and assign permissions to the roles
+
 module "datadog_monitors" {
   source = "../../"
 
--- examples/complete/outputs.tf
+++ examples/complete/outputs.tf
@@ -2,3 +2,8 @@ output "datadog_monitor_names" {
   value       = module.datadog_monitors.datadog_monitor_names
   description = "Names of the created Datadog monitors"
 }
+
+output "datadog_permissions" {
+  value       = data.datadog_permissions.permissions
+  description = "Available Datadog permissions"
+}
