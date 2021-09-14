24052fb48f515b9d59dff3d972619fa88f6064ea
nitrocode <nitrocode@users.noreply.github.com>
2021-09-14 16:15:12 -0400

    Update main.tf

--- modules/organization/main.tf
+++ modules/organization/main.tf
@@ -3,9 +3,12 @@ locals {
 }
 
 resource "datadog_child_organization" "default" {
-  count       = local.enabled ? 1 : 0
-  name        = module.this.id
-  description = var.description
-  settings    = var.settings
-  user        = var.user
+  count = local.enabled ? 1 : 0
+  name  = module.this.id
+}
+
+resource "datadog_organization_settings" "default" {
+  count    = local.enabled ? 1 : 0
+  name     = module.this.id
+  settings = var.settings
 }
