a109bf8194c6a6bb93c6ce775484709e40e34817
nitrocode <nitrocode@users.noreply.github.com>
2021-09-14 16:15:32 -0400

    Update versions.tf

--- modules/organization/versions.tf
+++ modules/organization/versions.tf
@@ -2,14 +2,6 @@ terraform {
   required_version = ">= 0.13.0"
 
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 2.0"
-    }
-    local = {
-      source  = "hashicorp/local"
-      version = ">= 1.3"
-    }
     datadog = {
       source  = "datadog/datadog"
       version = ">= 3.0.0"
