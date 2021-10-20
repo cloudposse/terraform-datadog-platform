f9bd918c339e85f91f087ebd8da1e9e8087d4057
Benjamin Smith <ben.smith.developer@gmail.com>
2021-10-20 15:12:09 -0700

    cleanup

--- modules/slo/variables.tf
+++ modules/slo/variables.tf
@@ -18,15 +18,3 @@ variable "datadog_slos" {
   }))
   description = "Map of Datadog monitor configurations. See catalog for examples"
 }
-#
-#variable "alert_tags" {
-#  type        = list(string)
-#  description = "List of alert tags to add to all alert messages, e.g. `[\"@opsgenie\"]` or `[\"@devops\", \"@opsgenie\"]`"
-#  default     = null
-#}
-#
-#variable "alert_tags_separator" {
-#  type        = string
-#  description = "Separator for the alert tags. All strings from the `alert_tags` variable will be joined into one string using the separator and then added to the alert message"
-#  default     = "\n"
-#}
