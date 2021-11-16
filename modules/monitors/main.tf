6efb0a9a831f71435e965cd7f9500fcc1bd90dca
Benjamin Smith <ben.smith.developer@gmail.com>
2021-11-16 10:26:00 -0800

    add priority as variable to monitor

--- modules/monitors/main.tf
+++ modules/monitors/main.tf
@@ -23,6 +23,7 @@ resource "datadog_monitor" "default" {
   include_tags        = lookup(each.value, "include_tags", null)
   enable_logs_sample  = lookup(each.value, "enable_logs_sample", null)
   force_delete        = lookup(each.value, "force_delete", null)
+  priority            = lookup(each.value, "priority", null)
 
   monitor_thresholds {
     warning           = lookup(each.value.thresholds, "warning", null)
