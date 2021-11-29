99fd8d237d46d820e6eef01df7dc83e1257fbf8c
nitrocode <nitrocode@users.noreply.github.com>
2021-11-29 11:33:28 -0600

    Bump aws and drop local provider

--- examples/synthetics/versions.tf
+++ examples/synthetics/versions.tf
@@ -4,11 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 2.0"
-    }
-    local = {
-      source  = "hashicorp/local"
-      version = ">= 1.3"
+      version = ">= 3.0"
     }
     datadog = {
       source  = "datadog/datadog"
