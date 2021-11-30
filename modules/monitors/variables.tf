55c0e9365a2be1305ada9346e3375d8e9cb745ce
nitrocode <nitrocode@users.noreply.github.com>
2021-11-30 11:41:25 -0600

    Missing var.datadog_monitors keys

--- modules/monitors/variables.tf
+++ modules/monitors/variables.tf
@@ -1,27 +1,32 @@
 # https://registry.terraform.io/providers/DataDog/datadog/latest/docs/resources/monitor
 variable "datadog_monitors" {
   type = map(object({
-    name                = string
-    type                = string
-    message             = string
-    escalation_message  = string
-    query               = string
-    tags                = list(string)
-    notify_no_data      = bool
-    new_host_delay      = number
-    evaluation_delay    = number
-    no_data_timeframe   = number
-    renotify_interval   = number
-    notify_audit        = bool
-    timeout_h           = number
-    enable_logs_sample  = bool
-    include_tags        = bool
-    require_full_window = bool
-    locked              = bool
-    force_delete        = bool
-    threshold_windows   = map(any)
-    thresholds          = map(any)
-    priority            = number
+    name                   = string
+    type                   = string
+    message                = string
+    escalation_message     = string
+    query                  = string
+    tags                   = list(string)
+    notify_no_data         = bool
+    new_host_delay         = number
+    new_group_delay        = number
+    evaluation_delay       = number
+    no_data_timeframe      = number
+    renotify_interval      = number
+    renotify_occurrences   = number
+    renotify_statuses      = set(string)
+    notify_audit           = bool
+    timeout_h              = number
+    enable_logs_sample     = bool
+    include_tags           = bool
+    require_full_window    = bool
+    locked                 = bool
+    force_delete           = bool
+    threshold_windows      = map(any)
+    thresholds             = map(any)
+    priority               = number
+    groupby_simple_monitor = bool
+    validate               = bool
   }))
   description = "Map of Datadog monitor configurations. See catalog for examples"
 }
