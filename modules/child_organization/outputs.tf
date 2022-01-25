8a6abd5f002b402353143bca328860e86dd341b3
Andriy Knysh <aknysh@users.noreply.github.com>
2022-01-24 23:56:56 -0500

    Fix `child_organization` outputs (#57)

--- modules/child_organization/outputs.tf
+++ modules/child_organization/outputs.tf
@@ -14,23 +14,23 @@ output "public_id" {
 }
 
 output "user" {
-  value       = try(datadog_child_organization.default[0].user, {})
+  value       = try(datadog_child_organization.default[0].user[0], {})
   description = "Information about organization users"
 }
 
 output "settings" {
-  value       = try(datadog_child_organization.default[0].settings, {})
+  value       = try(datadog_child_organization.default[0].settings[0], {})
   description = "Organization settings"
 }
 
 output "api_key" {
-  value       = try(datadog_child_organization.default[0].api_key, {})
+  value       = try(datadog_child_organization.default[0].api_key[0], {})
   description = "Information about Datadog API key"
   sensitive   = true
 }
 
 output "application_key" {
-  value       = try(datadog_child_organization.default[0].application_key, {})
+  value       = try(datadog_child_organization.default[0].application_key[0], {})
   description = "Datadog application key with its associated metadata"
   sensitive   = true
 }
