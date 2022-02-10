bc32a2b9a9e82fad83b082a7442a3cac74429ada
cloudpossebot <11232728+cloudpossebot@users.noreply.github.com>
2022-02-10 19:15:16 +0000

    Auto Format

--- modules/monitors/variables.tf
+++ modules/monitors/variables.tf
@@ -1,37 +1,37 @@
 # https://registry.terraform.io/providers/DataDog/datadog/latest/docs/resources/monitor
 variable "datadog_monitors" {
   type = any
-#    enabled                = bool
-#    name                   = string
-#    type                   = string
-#    message                = string
-#    escalation_message     = string
-#    query                  = string
-#    tags                   = map(string)
-#    notify_no_data         = bool
-#    new_group_delay        = number
-#    evaluation_delay       = number
-#    no_data_timeframe      = number
-#    renotify_interval      = number
-#    renotify_occurrences   = number
-#    renotify_statuses      = set(string)
-#    notify_audit           = bool
-#    timeout_h              = number
-#    enable_logs_sample     = bool
-#    include_tags           = bool
-#    require_full_window    = bool
-#    locked                 = bool
-#    force_delete           = bool
-#    threshold_windows      = map(any)
-#    thresholds             = map(any)
-#    priority               = number
-#    groupby_simple_monitor = bool
-#    validate               = bool
+  #    enabled                = bool
+  #    name                   = string
+  #    type                   = string
+  #    message                = string
+  #    escalation_message     = string
+  #    query                  = string
+  #    tags                   = map(string)
+  #    notify_no_data         = bool
+  #    new_group_delay        = number
+  #    evaluation_delay       = number
+  #    no_data_timeframe      = number
+  #    renotify_interval      = number
+  #    renotify_occurrences   = number
+  #    renotify_statuses      = set(string)
+  #    notify_audit           = bool
+  #    timeout_h              = number
+  #    enable_logs_sample     = bool
+  #    include_tags           = bool
+  #    require_full_window    = bool
+  #    locked                 = bool
+  #    force_delete           = bool
+  #    threshold_windows      = map(any)
+  #    thresholds             = map(any)
+  #    priority               = number
+  #    groupby_simple_monitor = bool
+  #    validate               = bool
 
-    # TODO: deprecate in favor of new_group_delay once the options are fully clarified
-    # See https://github.com/DataDog/terraform-provider-datadog/issues/1292
-#    new_host_delay = number
-#  }))
+  # TODO: deprecate in favor of new_group_delay once the options are fully clarified
+  # See https://github.com/DataDog/terraform-provider-datadog/issues/1292
+  #    new_host_delay = number
+  #  }))
   description = "Map of Datadog monitor configurations. See catalog for examples"
 }
 
