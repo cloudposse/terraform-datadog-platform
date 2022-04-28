93429df636939e3509890c006d801e30439255c9
Benjamin Smith <ben.smith.developer@gmail.com>
2022-04-28 13:01:03 -0700

    remove synthetics and slos from complete

--- examples/complete/variables.tf
+++ examples/complete/variables.tf
@@ -17,13 +17,3 @@ variable "monitor_paths" {
   type        = list(string)
   description = "List of paths to Datadog monitor configurations"
 }
-
-variable "slo_paths" {
-  type        = list(string)
-  description = "List of paths to Datadog SLO configurations"
-}
-
-variable "synthetic_paths" {
-  type        = list(string)
-  description = "List of paths to Datadog synthetic configurations"
-}
