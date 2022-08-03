c455d8405ee0306e7b6b0b5f7355f521898ffcfe
a296Cg <106979908+a296Cg@users.noreply.github.com>
2022-08-03 18:02:47 -0400

    remove unused provider and update minimum terraform version

--- modules/monitors/versions.tf
+++ modules/monitors/versions.tf
@@ -1,11 +1,7 @@
 terraform {
-  required_version = ">= 0.13.0"
+  required_version = ">= 1.0"
 
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 3.0"
-    }
     datadog = {
       source  = "datadog/datadog"
       version = ">= 3.0.0"
