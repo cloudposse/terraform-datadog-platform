c5218fc9767db74cc02c80772d400d0d6e38d1c8
Nuru <Nuru@users.noreply.github.com>
2024-10-25 00:29:46 -0700

    Update Datadog provider version requirement

--- modules/synthetics/versions.tf
+++ modules/synthetics/versions.tf
@@ -4,7 +4,8 @@ terraform {
   required_providers {
     datadog = {
       source  = "datadog/datadog"
-      version = ">= 3.31.0"
+      # Must have >= 3.43.1 to have fix for https://github.com/DataDog/terraform-provider-datadog/issues/2531
+      version = ">= 3.43.1"
     }
   }
 }
