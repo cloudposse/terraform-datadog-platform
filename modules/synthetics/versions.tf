c04a852d848a75496b2f46330cf790b23a6d153f
elx-bob <95710776+elx-bob@users.noreply.github.com>
2024-10-25 02:50:14 -0500

    fix(synthetics): Adjust timeframes loop to include child objects (#107)

--- modules/synthetics/versions.tf
+++ modules/synthetics/versions.tf
@@ -3,8 +3,9 @@ terraform {
 
   required_providers {
     datadog = {
-      source  = "datadog/datadog"
-      version = ">= 3.31.0"
+      source = "datadog/datadog"
+      # Must have >= 3.43.1 to have fix for https://github.com/DataDog/terraform-provider-datadog/issues/2531
+      version = ">= 3.43.1"
     }
   }
 }
